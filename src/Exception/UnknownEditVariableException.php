a3a67beb5cc517ebda35fc3c513f4ea16527db08
Stefan Szymanski <ctefan.szymanski@gmail.com>
2018-10-23 10:00:32 +0200

    Add declare(strict_types=1); to all PHP files

--- src/Exception/UnknownEditVariableException.php
+++ src/Exception/UnknownEditVariableException.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: stefan.szymanski
- * Date: 22.10.18
- * Time: 10:19
- */
+declare(strict_types=1);
 
 namespace Ctefan\Kiwi\Exception;
-
 
 class UnknownEditVariableException extends Exception
 {
